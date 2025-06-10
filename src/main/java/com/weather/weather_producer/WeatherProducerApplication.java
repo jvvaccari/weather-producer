bc4d6b777ee2387678fc571f333b3fd93aa08dc8
jvvaccari <jv_vaccari@live.com>
2025-06-09 22:53:26 -0300

    Subindo meu primeiro commit

---
+++ src/main/java/com/weather/weather_producer/WeatherProducerApplication.java
@@ -0,0 +1,13 @@
+package com.weather.weather_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherProducerApplication.class, args);
+	}
+
+}
